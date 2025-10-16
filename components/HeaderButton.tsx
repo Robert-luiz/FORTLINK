ce2cf4b713d1f67fae38d193561ad07bbb44f21d
Robert-luiz <roluiz789@gmail.com>
2025-10-16 13:13:35 -0300

    tirei a API do CEP

--- components/HeaderButton.tsx
+++ components/HeaderButton.tsx
@@ -9,13 +9,9 @@ export default function HeaderButton({ onClick, children }: HeaderButtonProps) {
   return (
     <button
       onClick={onClick}
-      className="relative inline-flex items-center justify-center px-6 py-2 overflow-hidden font-bold text-white uppercase transition-all duration-300 ease-in-out rounded-lg group"
+      className="bg-[#00B723] inline-flex items-center justify-center px-6 py-2 overflow-hidden font-bold text-white uppercase transition ease-in-out duration-500 rounded-lg hover:transition hover:duration-300 hover:cursor-pointer group hover:bg-gradient-to-r from-[#00B723] to-[#00D109] hover:shadow-lg hover:shadow-[#00D109]/30"
     >
-      <span className="absolute top-0 left-0 w-full h-full bg-gradient-to-br from-[#00D109] to-[#00B723] rounded-lg"></span>
-      <span className="absolute bottom-0 right-0 w-full h-full transition-all duration-500 ease-in-out transform scale-0 bg-black rounded-lg group-hover:scale-100"></span>
-      <span className="relative text-black group-hover:text-white transition-colors duration-300">
         {children}
-      </span>
     </button>
   );
 }
